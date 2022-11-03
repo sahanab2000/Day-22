fe626f09d4b100919bc33ec2d64a3c8d228056ce
sahanab2000 <85185990+sahanab2000@users.noreply.github.com>
2022-11-03 11:53:28 +0530

    Add files via upload

---
+++ ReflectionOnValidateFirstName.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day22ReflectionsUserRegistration
+{
+    internal class ReflectionOnValidateFirstName
+    {
+        public static void ReflectionTest()
+        {
+            Type type = Type.GetType("Day22ReflectionsUserRegistration.UC1ValidateFirstName");
+            Console.WriteLine("Full Name :{0}", type.FullName);
+            Console.WriteLine("Class name is : {0}", type.Name);
+            Console.WriteLine("Methods in customerInfo class");
+            MethodInfo[] methods = type.GetMethods();
+            foreach (MethodInfo method in methods)
+            {
+                Console.WriteLine(method.ReturnType.Name + " " + method.Name);
+            }
+
+        }
+    }
+}
---
+++ UC1ValidateFirstName.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day22ReflectionsUserRegistration
+{
+    internal class UC1ValidateFirstName
+    {
+        //method to validate the first name
+        public void CheckFirstName()
+        {
+            //first name regex
+            String nameRegex = "^[A-Z][a-zA-Z]{2,}"; 
+            Console.Write("Enter the First Name: ");
+            String userName = Console.ReadLine();
+            //compare the name regex with the user entered first name
+            bool valid = Regex.IsMatch(userName, nameRegex);
+            Console.WriteLine(valid);
+        }
+    }
+}
